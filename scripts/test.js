173e8f0f1414aa4fd4ae4b5188f04322f63c4401
Drake Sage <sageda1@s.dcsdk12.org>
2025-09-26 16:25:49 -0600

    added no-damage attacks to test

--- scripts/test.js
+++ scripts/test.js
@@ -1,56 +1,20 @@
 //@ts-check
 const ctx = document.getElementById("screen")
+//@ts-ignore
 const c = ctx.getContext("2d")
+let x2 = 0, y2 = 0, type = "", num = 0;
+let fps = 60
+const width = 1280
+const height = 640
+//I know this array is a sloppy form of objects, but I'm used to doing this
+let attacks = [640, 320, "laser", Math.PI * 1.75, 1240, 40, "projectile", Math.PI * 1.625]
 let rgb = "#ffffff"
 let rgb2 = "#000000"
 let fl = true
 let st = false
-const fill = function(r, g, b) {
-    fl = true
-    rgb = "#" + r.toString(16) + g.toString(16) + b.toString(16)
-}
-const stroke = function(r, g, b) {
-    st = true
-    rgb2 = r + g + b
-}
-const noFill = function() {
-    fl = false
-}
-const noStroke = function() {
-    st = false
-}
-const rect = function(x, y, x2, y2) {
-    if(fl){
-        c.fillStyle = rgb
-    }
-    if(st){
-        c.strokeStyle = rgb2
-    }
-    c.moveTo(x, y)
-    c.lineTo(x+x2, y)
-    c.lineTo(x+x2, y+y2)
-    c.lineTo(x, y+y2)
-    c.lineTo(x, y)
-    c.closePath()
-    if (fl){
-        c.fill()
-    }
-    if(st){
-        c.stroke()
-    }
-}
-
-
-
-
-//behind here is code to turn this into processing java
-
-
-
-
-setTimeout(() => {draw()}, 100);
 let up, down, left, right = false;
 let x = 1, y = 1;
+let fpsCalc = 1000/60
 document.addEventListener("keydown", (event) => {
     console.log(event.key);
     if (event.key == "ArrowUp" || event.code == "KeyW") {
@@ -89,36 +53,113 @@ document.addEventListener("keyup", (event) => {
         down = false;
     }
 })
-const draw = function() {
-    setTimeout(() => {draw();}, 100)
+const draw = function(timestamp) {
+    fps = 1000/(timestamp-fpsCalc)
     if (up && !down && !((left || right) && !(left && right))){
-        y--
+        y-= 320/fps
     }
     if (up && !down && ((left || right) && !(left && right))){
-        y-= Math.sqrt(0.5)
+        y-= Math.sqrt((320*320)/2)/fps
     }
     if (down && !up && !((left || right) && !(left && right))){
-        y++
+        y+= 320/fps
     }
     if (down && !up && ((left || right) && !(left && right))){
-        y+= Math.sqrt(0.5)
+        y+= Math.sqrt((320*320)/2)/fps
     }
     if (left && !right && !((up || down) && !(up && down))){
-        x--
+        x-= 320/fps
     }
     if (left && !right && ((up || down) && !(up && down))){
-        x-= Math.sqrt(0.5)
+        x-= Math.sqrt((320*320)/2)/fps
     }
     if (right && !left && !((up || down) && !(up && down))){
-        x++
+        x+= 320/fps
     }
     if (right && !left && ((up || down) && !(up && down))){
-        x+= Math.sqrt(0.5)
+        x+= Math.sqrt((320*320)/2)/fps
     }
-    console.log(x)
-    console.log(y)
-    fill("ff", "ff", "ff")
-    setTimeout(() => {rect(0, 0, 1280, 640)}, 10)
-    setTimeout(() => {fill("80", "80", "80")}, 20)
-    setTimeout(() => {rect(x, y, 100, 100)}, 30)
+    function projectile(x2, y2, num, i) {
+        if (x2 <= 20 || x2 >= width - 20) {
+            num = num * -1
+            if (x2 >= width - 20){
+                x2 = width - 20
+            }else{
+                x2 = 20
+            }
+        }
+        if (y2 <= 20 || y2 >= height - 20) {
+            num = Math.PI - num
+            if (y2 >= height - 20){
+                y2 = height - 20
+            }else{
+                y2 = 20
+            }
+        }
+        x2 += (100 * Math.sin(num))/fps; 
+        y2 += (100 * Math.cos(num))/fps; 
+        c.fillStyle = "#ff0000"; 
+        c.fillRect(x2-20, y2-20, 40, 40); 
+        attacks[i - 3] = x2
+        attacks[i - 2] = y2
+        attacks[i] = num
+    }
+    function laser(x2, y2, num, i) {
+        let num2 = num + Math.PI
+        c.beginPath(); 
+        c.moveTo(x2 + (Math.sin(num) * 10), y2 - (Math.cos(num) * 10));
+        c.lineTo(x2 + (Math.sin(num) * 10) + (Math.cos(num)*1000), y2 - (Math.cos(num) * 10) + (Math.sin(num)*1000))
+        c.lineTo(x2 + (Math.sin(num2) * 10) + (Math.cos(num)*1000), y2 - (Math.cos(num2) * 10) + (Math.sin(num)*1000))
+        c.lineTo(x2 + (Math.sin(num2) * 10) - (Math.cos(num)*1000), y2 - (Math.cos(num2) * 10) - (Math.sin(num)*1000))
+        c.lineTo(x2 + (Math.sin(num) * 10) - (Math.cos(num)*1000), y2 - (Math.cos(num) * 10) - (Math.sin(num)*1000))
+        c.closePath()
+        c.fillStyle = "#ff0000"
+        c.fill()
+        c.beginPath()
+        c.moveTo(x2 + (Math.sin(num) * 12) + (Math.cos(num)*10), y2 - (Math.cos(num) * 12) + (Math.sin(num)*10))
+        c.lineTo(x2 + (Math.sin(num) * 12) + (Math.cos(num)*10), y2 - (Math.cos(num) * 12) + (Math.sin(num)*10))
+        c.lineTo(x2 + (Math.sin(num2) * 12) + (Math.cos(num)*10), y2 - (Math.cos(num2) * 12) + (Math.sin(num)*10))
+        c.lineTo(x2 + (Math.sin(num2) * 12) - (Math.cos(num)*10), y2 - (Math.cos(num2) * 12) - (Math.sin(num)*10))
+        c.lineTo(x2 + (Math.sin(num) * 12) - (Math.cos(num)*10), y2 - (Math.cos(num) * 12) - (Math.sin(num)*10))
+        c.closePath()
+        c.fillStyle = "#000000"
+        c.fill()
+        attacks[i] += Math.PI * 0.1/fps
+    }
+    c.clearRect(0, 0, 1280, 640)
+    c.fillStyle = "#404040"
+    c.fillRect(0, 0, 1280, 640)
+    c.fillStyle = "#ff0000"
+    c.fillRect(x, y, 40, 40)
+    for (let i = 0; i <= attacks.length; i++){
+        if (i%4 == 0) {
+            x2 = attacks[i];
+        }else if (i%4 == 1) {
+            y2 = attacks[i]
+        }else if (i%4 == 2) {
+            type = attacks[i]
+        }else{
+            num = attacks[i]
+            if (type == "projectile"){
+                projectile(x2, y2, num, i)
+            }else if (type == "laser"){
+                laser(x2, y2, num, i)
+            }
+        }
+    }
+    if (timestamp >= 4000 && timestamp <= 4999){
+        c.fillStyle = "#b04040"
+        c.fillRect(width/4 - 10, -1, 20, height + 2)
+        c.fillStyle = "#404040"
+        c.fillRect(308, 790, 24, 20)
+    }
+    if (timestamp >= 5000 && attacks[8] == 0){
+        attacks[8] = width/4
+        attacks[9] = width/4*3
+        attacks[10] = "laser"
+        attacks[11] = 0
+    }
+    fpsCalc = timestamp
+    requestAnimationFrame(draw)
 }
+requestAnimationFrame(draw)
